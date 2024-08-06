f1b65a595e22dfe8819c36fcb1c6f223dd38acd0
Madhusudan Singh Chauhan <136247818+Madhusudan0012@users.noreply.github.com>
2024-08-07 00:06:06 +0530

    conversion

--- 03Conversion.js
+++ 03Conversion.js
@@ -14,3 +14,26 @@ let someNumber = 222
 let StringNumber = String(someNumber);
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+//**********************Operations******************* */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2+2);
+
+let str1 = "hello"
+let str2 = "Madhusudan Singh Chauhan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+2);
