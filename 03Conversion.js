6fa1d6f70682a89e376e9974d8afbf4af88db8e8
Madhusudan Singh Chauhan <136247818+Madhusudan0012@users.noreply.github.com>
2024-08-07 13:32:29 +0530

    comparesion

--- 03Conversion.js
+++ 03Conversion.js
@@ -37,3 +37,15 @@ console.log(str3);
 
 console.log("1" + 2);
 console.log(1+2);
+
+console.log("2" + 2+ 2)
+
+console.log(+true)
+
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameConunter = 100;
+gameConunter++;
+console.log(gameConunter);
